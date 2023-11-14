3d845642702d324bbcb9fcbeb511bfe380e1410f
sb8001at <sb8001at@users.noreply.github.com>
2023-11-14 11:11:10 +0900

    Update server.R

--- server.R
+++ server.R
@@ -1,13 +1,20 @@
 function(input, output, session) {
   
   mm_3lot <- reactive({return(mean(c(input$lot1mean, input$lot2mean, input$lot3mean)))})
-  svar_3lot <- reactive({return(calc_var(input$lot1sd, input$lot2sd, input$lot3sd, input$n_sample))})
-  ssd_3lot <- reactive({return((svar_3lot() ^ 0.5))})
+  ssd_3lot <- reactive({return(mean(c(input$lot1sd, input$lot2sd, input$lot3sd)))})
   ms_3lot <- reactive({return(ssd_3lot() / input$n_sample ^ 0.5)})
-  sixsigma <- reactive({return((ssd_3lot() * 3))}) # 名前はsixsigmaだが，開発中に3sigmaになっている．
+  sixsigma <- reactive({return((ssd_3lot() * 3))})
   m_seq <- reactive({return(m_seq_fn(mm_3lot(), sixsigma()))})
-  s_seq <- reactive({return(s_seq_fn(sixsigma()))})
-  
+  d_l <- reactive({return(calc_psd(input$n_sample, ssd_3lot()))})
+  intr <- reactive({return(
+    d_l() %>% .[[1]] %>% 
+      mutate(
+        col_90 = if_else(prob_ == 0.05 | prob_ == 0.95, var_^0.5, NA),
+        col_95 = if_else(prob_ == 0.025 | prob_ == 0.975, var_^0.5, NA),
+        col_99 = if_else(prob_ == 0.005 | prob_ == 0.995, var_^0.5, NA)
+      ) %>% 
+      filter(!is.na(col_90) | !is.na(col_95) | !is.na(col_99))
+  )})
   
   observeEvent(input$runCalc, {
     output$meandist_plot <- renderPlot(
@@ -81,13 +88,13 @@ function(input, output, session) {
     )
     
     output$sddist_plot <- renderPlot(
-      df_sddist_plot(s_seq(), svar_3lot())
+      df_sddist_plot(d_l(), intr(), ssd_3lot())
     )
     
     output$sddist_90 <- renderText(
       paste0(
         "90%のロットでは標準偏差は<span style=\"color:#99D6D1;font-weight:bold;\">", 
-        round(qchisq(0.05, svar_3lot())^0.5, digits = 2), "～", round(qchisq(0.95, svar_3lot())^0.5, digits = 2), 
+       round(intr()$col_90 %>% na.omit() %>% .[1], digits=2), "～", round(intr()$col_90 %>% na.omit() %>% .[2], digits=2), 
         "</span>%の範囲に含まれます。"
       )
     )
@@ -95,7 +102,7 @@ function(input, output, session) {
     output$sddist_95 <- renderText(
       paste0(
         "95%のロットでは標準偏差は<span style=\"color:#56bcb3;font-weight:bold;\">", 
-        round(qchisq(0.025, svar_3lot())^0.5, digits = 2), "～", round(qchisq(0.975, svar_3lot())^0.5, digits = 2), 
+        round(intr()$col_95 %>% na.omit() %>% .[1], digits=2), "～", round(intr()$col_95 %>% na.omit() %>% .[2], digits=2),  
         "</span>%の範囲に含まれます。"
       )
     )
@@ -103,14 +110,14 @@ function(input, output, session) {
     output$sddist_99 <- renderText(
       paste0(
         "99%のロットでは標準偏差は<span style=\"color:#2B5D59;font-weight:bold;\">", 
-        round(qchisq(0.005, svar_3lot())^0.5, digits = 2), "～", round(qchisq(0.995, svar_3lot())^0.5, digits = 2), 
+        round(intr()$col_99 %>% na.omit() %>% .[1], digits=2), "～", round(intr()$col_99 %>% na.omit() %>% .[2], digits=2), 
         "</span>%の範囲に含まれます。"
       )
     )
   })
   
   observeEvent(input$runSim, {
-    sim_u <- sim_uniform(mm_3lot(), ms_3lot(), svar_3lot())
+    sim_u <- sim_uniform(mm_3lot(), ms_3lot(), d_l())
     
     output$sim_3lot <- renderDT(
       sim_u[[1]] %>% round(digits=1), 
@@ -155,9 +162,110 @@ function(input, output, session) {
   })
   
   observeEvent(input$printControlChart, {
-    cc <- control_chart_fn(mm_3lot(), ms_3lot(), svar_3lot())
+    cc <- control_chart_fn(mm_3lot(), ms_3lot(), d_l())
     output$cchart <- renderPlot(cc[[2]])
     output$ACchart <- renderPlot(cc[[3]])
   })
+  
+  output$chisq_f <- renderUI(
+    withMathJax(
+      helpText("式1：以下の式は自由度kのカイ二乗分布に従う: $$ \\frac{k \\cdot s^2}{\\sigma^2} \\sim \\chi^2(k)$$")
+    )
+  )
+  
+  output$chisq_2 <- renderUI(
+    withMathJax(
+      helpText("式2：式1にn=10，s=2を代入する: $$ \\frac{9 \\cdot 4}{\\sigma^2} \\sim \\chi^2(9)$$")
+    )
+  )
+  
+  output$chisq_p9 <- renderPlot(
+    plot(x = qchisq(seq(0.0001, 1, by=0.0001), 9), y = seq(0.0001, 1, by=0.0001), type="l", xlab="値", ylab="累積確率")
+  )
+  
+  output$chisq_psd <- renderPlot({
+    # サンプル数
+    n = 10
+    # 標本標準偏差
+    s = 2
+    # 偏差平方和（分散 × 自由度）
+    ss <- s ^ 2 * (n-1)
+    # 確率を示す数値
+    s_seq <- seq(0.0001, 1, by=0.0001)
+    # 自由度n-1の累積確率密度（偏差平方和/母分散の累積確率密度）
+    q_chisq <- qchisq(s_seq, n-1)
+    # 母分散の累積確率密度（上のq_chisqと偏差平方和から計算）
+    sig <- ss/q_chisq
+    # 母分散（母分散の平方根）の累積確率密度をプロットしたもの
+    plot(sig ^ 0.5, s_seq, type="l", xlab="母標準偏差", ylab="累積確率密度（を1から引いたもの）")
+  })
+  
+  output$dist_psd <- renderPlot({
+    # サンプル数
+    n = 10
+    # 標本標準偏差
+    s = 2
+    # 偏差平方和（分散 × 自由度）
+    ss <- s ^ 2 * (n-1)
+    # 確率を示す数値
+    s_seq <- seq(0.0001, 1, by=0.0001)
+    # 自由度n-1の累積確率密度（偏差平方和/母分散の累積確率密度）
+    q_chisq <- qchisq(s_seq, n-1)
+    # 母分散の累積確率密度（上のq_chisqと偏差平方和から計算）
+    sig <- ss/q_chisq
+    # 累積確率密度を微分してマイナスを付けたもの
+    plot(((sig[-1] + sig[-length(sig)])/2)^0.5, -diff(s_seq)/diff(sig), type="l", xlab="母標準偏差", ylab="確率密度関数（のようなもの）")
+  })
+    
+  output$dist_psd_point <- renderPlot({
+    # サンプル数
+    n = 10
+    # 標本標準偏差
+    s = 2
+    # 偏差平方和（分散 × 自由度）
+    ss <- s ^ 2 * (n-1)
+    # 確率を示す数値
+    s_seq <- seq(0.0001, 1, by=0.0001)
+    # 自由度n-1の累積確率密度（偏差平方和/母分散の累積確率密度）
+    q_chisq <- qchisq(s_seq, n-1)
+    # 母分散の累積確率密度（上のq_chisqと偏差平方和から計算）
+    sig <- ss/q_chisq
+    # 累積確率密度を微分してマイナスを付けたもの
+    plot(x=(((sig[-1] + sig[-length(sig)])/2)^0.5), y=(-diff(s_seq)/diff(sig)), xlab="母標準偏差", ylab="確率密度関数（のようなもの）", cex=0.5)
+  })
+  
+  output$dist_psd_dist <- renderPlot({
+    # サンプル数
+    n = 10
+    # 標本標準偏差
+    s = 2
+    # 偏差平方和（分散 × 自由度）
+    ss <- s ^ 2 * (n-1)
+    # 確率を示す数値
+    s_seq <- seq(0.0001, 1, by=0.0001)
+    # 自由度n-1の累積確率密度（偏差平方和/母分散の累積確率密度）
+    q_chisq <- qchisq(s_seq, n-1)
+    # 母分散の累積確率密度（上のq_chisqと偏差平方和から計算）
+    sig <- ss/q_chisq
+    # 累積確率密度を微分してマイナスを付けたもの
+    plot(x=(((sig[-1] + sig[-length(sig)])/2)^0.5)[-1], y=-diff(((sig[-1] + sig[-length(sig)])/2)^0.5), xlab="母標準偏差", ylab="母標準偏差の横の幅", type="l")
+  })  
+ 
+  output$dist_psd_dist2 <- renderPlot({
+    # サンプル数
+    n = 10
+    # 標本標準偏差
+    s = 2
+    # 偏差平方和（分散 × 自由度）
+    ss <- s ^ 2 * (n-1)
+    # 確率を示す数値
+    s_seq <- seq(0.0001, 1, by=0.0001)
+    # 自由度n-1の累積確率密度（偏差平方和/母分散の累積確率密度）
+    q_chisq <- qchisq(s_seq, n-1)
+    # 母分散の累積確率密度（上のq_chisqと偏差平方和から計算）
+    sig <- ss/q_chisq
+    # 累積確率密度を微分してマイナスを付けたもの
+    plot(x=(((sig[-1] + sig[-length(sig)])/2)^0.5)[-1], y=-diff(((sig[-1] + sig[-length(sig)])/2)^0.5), xlab="母標準偏差", ylab="母標準偏差の横の幅", ylim=c(0, 0.01), type="l")
+  })   
   
 }
